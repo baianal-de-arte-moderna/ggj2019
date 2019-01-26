4520c14d0e5cf2358307b2bfa20dd3a97c9b2275
Piramba <avpaganini@gmail.com>
2019-01-25 22:15:08 -0200

    Simple Camera Follow

--- HoneyIAmHome/Assets/Scripts/FollowPlayerScript.cs
+++ HoneyIAmHome/Assets/Scripts/FollowPlayerScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FollowPlayerScript : MonoBehaviour
 {
+    public Transform Player;
+    public Vector3 Offset;
+
+    Transform m_transform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_transform.position = Vector3.Lerp(m_transform.position, Player.position + Offset, 0.1f);
+        m_transform.LookAt(Player);
     }
 }
