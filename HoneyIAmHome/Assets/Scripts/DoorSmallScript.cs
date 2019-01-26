4d8d011e3407739faeed1cd25587ca39245ea3f5
Vinicius Uzeda <vruzeda@gmail.com>
2019-01-26 12:12:00 -0200

    Animate small door

---
+++ HoneyIAmHome/Assets/Scripts/DoorSmallScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSmallScript : MonoBehaviour
+{
+    public Animator animator;
+    public string parameterName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool(parameterName, true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool(parameterName, false);
+    }
+}
