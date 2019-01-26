beefe95205d6897f133051ed57be5783da28318f
Barbara Maximo <barbara.cmaximo@gmail.com>
2019-01-26 01:01:51 -0200

    level and maxlevel

---
+++ HoneyIAmHome/Assets/Scripts/StoveEventScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveEventScript : ObjectEventScript
+{
+    protected override void OnLevelUp(int level)
+    {
+        Debug.Log("Level Up!");
+    }
+
+    protected override void OnGameOver()
+    {
+        Debug.Log("Game Over :c");
+    }
+}
