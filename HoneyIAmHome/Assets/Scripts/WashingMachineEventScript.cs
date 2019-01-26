214e98e5ddccbf13365eb4947cab2992f82df769
Vinicius Uzeda <vruzeda@gmail.com>
2019-01-26 08:21:25 -0200

    Animate to washing machine

--- HoneyIAmHome/Assets/Scripts/WashingMachineEventScript.cs
+++ HoneyIAmHome/Assets/Scripts/WashingMachineEventScript.cs
@@ -6,6 +6,7 @@ public class WashingMachineEventScript : ObjectEventScript
 {
     public MeshRenderer fixedCover;
     public MeshRenderer brokenCover;
+    public Animator doorAnimator;
 
     protected override void OnGameOver()
     {
@@ -16,6 +17,7 @@ public class WashingMachineEventScript : ObjectEventScript
     {
         fixedCover.enabled = true;
         brokenCover.enabled = false;
+        doorAnimator.SetTrigger("Fix");
     }
 
     protected override void OnLevelUp(int level)
@@ -29,6 +31,7 @@ public class WashingMachineEventScript : ObjectEventScript
             case 1:
                 fixedCover.enabled = false;
                 brokenCover.enabled = true;
+                doorAnimator.SetTrigger("Break");
                 break;
             case 2:
                 Debug.Log("BOOOM!");
