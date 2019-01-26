5a6f2f5f0d0fc4bf85163b7e364b9251251a8f0a
Vinicius Uzeda <vruzeda@gmail.com>
2019-01-26 00:24:40 -0200

    Implement player interaction script, and an initial version of fire extinguisher

---
+++ HoneyIAmHome/Assets/Scripts/PlayerInteractionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionScript : MonoBehaviour
+{
+    private InteractableObjectScript nearestInteractableObject;
+
+    private void Update()
+    {
+        if (Input.GetAxis("Interact") > 0f)
+        {
+            if (nearestInteractableObject != null)
+            {
+                nearestInteractableObject.Interact();
+            }
+        }
+    }
+
+    public void OnInteractableObjectTriggerEnter(InteractableObjectScript interactableObject)
+    {
+        nearestInteractableObject = interactableObject;
+    }
+
+    public void OnInteractableObjectTriggerExit(InteractableObjectScript interactableObject)
+    {
+        if (nearestInteractableObject == interactableObject)
+        {
+            nearestInteractableObject = null;
+        }
+    }
+}
