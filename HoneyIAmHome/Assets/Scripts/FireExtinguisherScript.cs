ad42dd0bf5a499fd2f4ede94046f72ad5a8f5ac0
Vinicius Uzeda <vruzeda@gmail.com>
2019-01-27 03:51:35 -0200

    Add particles to fire extinguisher

--- HoneyIAmHome/Assets/Scripts/FireExtinguisherScript.cs
+++ HoneyIAmHome/Assets/Scripts/FireExtinguisherScript.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class FireExtinguisherScript : ObjectGrabbingScript
 {
+    public ParticleSystem foam;
+
     public override void InteractWith(ObjectInteractionScript interactableObject)
     {
         interactableObject.InteractWithFireExtinguisher();
+    }
+
+    public override void OnInteractionStart()
+    {
+        Debug.Log("Start foaming!");
+        foam.Play();
+    }
+
+    public override void OnInteractionEnd()
+    {
+        Debug.Log("Stop foaming!");
+        foam.Stop();
     }
 }
