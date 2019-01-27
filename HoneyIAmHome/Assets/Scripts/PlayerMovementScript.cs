dae476c2d4aeea5fb156e44e56e57f4d17871d2e
Piramba <avpaganini@gmail.com>
2019-01-27 05:46:06 -0200

    Personagem e animacao

--- HoneyIAmHome/Assets/Scripts/PlayerMovementScript.cs
+++ HoneyIAmHome/Assets/Scripts/PlayerMovementScript.cs
@@ -7,10 +7,14 @@ public class PlayerMovementScript : MonoBehaviour
     public float speed;
 
     private Rigidbody rb;
+    private Animator animator;
+    private Transform m_transform;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        m_transform = GetComponent<Transform>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void FixedUpdate()
@@ -21,6 +25,10 @@ public class PlayerMovementScript : MonoBehaviour
         if (moveVertical != 0f || moveHorizontal != 0f) {
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
             rb.velocity = (movement * speed);
+            m_transform.rotation = Quaternion.LookRotation(rb.velocity);
+            animator.SetBool("Walk", true);
+        } else {
+            animator.SetBool("Walk", false);
         }
     }
 }
