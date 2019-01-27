af10ed9b041fd7ccbf215be6dbf24af08abfc857
Piramba <avpaganini@gmail.com>
2019-01-27 08:54:35 -0200

    Som das portas

--- HoneyIAmHome/Assets/Scripts/DoorSmallScript.cs
+++ HoneyIAmHome/Assets/Scripts/DoorSmallScript.cs
@@ -6,14 +6,25 @@ public class DoorSmallScript : MonoBehaviour
 {
     public Animator animator;
     public string parameterName;
+    private AudioSource aSource;
+
+    public AudioClip doorOpen;
+    public AudioClip doorClose;
+
+    void Start()
+    {
+        aSource = GetComponentInParent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         animator.SetBool(parameterName, true);
+        aSource.PlayOneShot(doorOpen);
     }
 
     private void OnTriggerExit(Collider other)
     {
         animator.SetBool(parameterName, false);
+        aSource.PlayOneShot(doorClose);
     }
 }
